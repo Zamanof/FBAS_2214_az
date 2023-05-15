4d4b019f1a9455c89a4fef85659ea846024d97eb
zamanof <nadir.r.zamanov@gmail.com>
2023-05-15 22:17:55 +0400

    getPlanet

--- React lessons/sw_react/src/Services/api-service.js
+++ React lessons/sw_react/src/Services/api-service.js
@@ -1,1 +1,45 @@
+class ApiService {
+    _apiBase = "https://swapi.py4e.com/api"
+    async getResources(url){
+        const responce = await fetch(`${this._apiBase}${url}`)
+        if (!responce.ok){
+            throw new Error(`Could not fetch ${url}, recived ${responce.status}`)
+        }
+        return await responce.json()
+    }
 
+    async getPeople(){
+        const responce = await this.getResources(`/people/`)
+        return responce.results
+    }
+    getPerson(id){
+        return this.getResources(`/people/${id}/`).result
+
+    }
+
+    async getAllPlanets(){
+        const responce = await this.getResources(`/planets/`)
+        return responce.results
+    }
+    getPlanet(id){
+        return this.getResources(`/planets/${id}/`)
+
+    }
+
+    async getAllStarships(){
+        const responce = await this.getResources(`/starships/`)
+        return responce.results
+    }
+    getStarship(id){
+        return this.getResources(`/starships/${id}/`)
+
+    }
+}
+
+export default ApiService;
+
+
+// fetch("https://www.swapi.tech/api/people/1/")
+//     .then((res)=>res.json())
+//     .then((data)=>{
+//         console.log(data.result.properties.name)})
--- React lessons/sw_react/src/components/random-planet/random-planet.js
+++ React lessons/sw_react/src/components/random-planet/random-planet.js
@@ -1,25 +1,50 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./random-planet.css";
+import ApiService from "../../Services/api-service"
 
 const RandomPlanet = () => {
+    const apiService = new ApiService()
+    const [planet, setPlanet] = useState({
+        id: null,
+        name:null,
+        population:null,
+        rotationPeriod:null,
+        diameter:null
+    })
+
+    const updatePlanet = ()=>{
+        let id = 10
+        apiService.getPlanet(id)
+            .then((planet)=>{
+                setPlanet({
+                id,
+                name: planet.name,
+                population: planet.population,
+                rotationPeriod: planet.rotation_period,
+                diameter: planet.diameter
+            })})
+    }
+    useEffect(updatePlanet, [])
+
     return (
-        <div className="random-planet jumbotron rounded">
+        <div className="random-planet bg-dark rounded">
           <img className="planet-image"
-               src="https://starwars-visualguide.com/assets/img/planets/5.jpg" />
+               src="https://starwars-visualguide.com/assets/img/planets/6.jpg"
+          alt="planet images"/>
           <div>
-            <h4>Planet Name</h4>
+            <h4>{planet.name}</h4>
             <ul className="list-group list-group-flush">
               <li className="list-group-item">
                 <span className="term">Population</span>
-                <span>123124</span>
+                <span>{planet.population}</span>
               </li>
               <li className="list-group-item">
                 <span className="term">Rotation Period</span>
-                <span>43</span>
+                <span>{planet.rotationPeriod}</span>
               </li>
               <li className="list-group-item">
                 <span className="term">Diameter</span>
-                <span>100</span>
+                <span>{planet.diameter}</span>
               </li>
             </ul>
           </div>
